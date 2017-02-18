65b06976b222cbf2024d602f8450c5f7a1f2c97b
xpheal <xpheal@gmail.com>
2017-02-17 21:26:19 -0600

    title

--- plot.py
+++ plot.py
@@ -23,18 +23,22 @@ with open('out.data', 'r') as file:
 
 e = [x for x in range(0, 3000)]
 
-train[0] = [float(x[18:]) for x in train[0]]
-tune[0] = [float(x[18:]) for x in tune[0]]
-test[0] = [float(x[18:]) for x in test[0]]
-train[1] = [float(x[19:]) for x in train[1]]
-tune[1] = [float(x[19:]) for x in tune[1]]
-test[1] = [float(x[19:]) for x in test[1]]
-train[2] = [float(x[18:]) for x in train[2]]
-tune[2] = [float(x[18:]) for x in tune[2]]
-test[2] = [float(x[18:]) for x in test[2]]
-train[3] = [float(x[18:]) for x in train[3]]
-tune[3] = [float(x[18:]) for x in tune[3]]
-test[3] = [float(x[18:]) for x in test[3]]
+train[0] = [float(x[18:]) * 100 for x in train[0]]
+tune[0] = [float(x[18:]) * 100 for x in tune[0]]
+test[0] = [float(x[18:]) * 100 for x in test[0]]
+train[1] = [float(x[19:]) * 100 for x in train[1]]
+tune[1] = [float(x[19:]) * 100 for x in tune[1]]
+test[1] = [float(x[19:]) * 100 for x in test[1]]
+train[2] = [float(x[18:]) * 100 for x in train[2]]
+tune[2] = [float(x[18:]) * 100 for x in tune[2]]
+test[2] = [float(x[18:]) * 100 for x in test[2]]
+train[3] = [float(x[18:]) * 100 for x in train[3]]
+tune[3] = [float(x[18:]) * 100 for x in tune[3]]
+test[3] = [float(x[18:]) * 100 for x in test[3]]
+
+plt.title('Overall Accuracy versus Epoch(time)')
+plt.ylabel('Accuracy (%)')
+plt.xlabel('Epoch / Time')
 
 plt.plot(e, train[3], 'red')
 plt.plot(e, tune[3], 'blue')
